25eeca04149ce7e4edfb9ca3c5ee3f10212cb386
Giuliano Gusmão <gusmaoggg@gmail.com>
2018-06-25 20:39:18 -0300

    Criando formulário e cadastrando um novo produto

--- app/infra/produtosDAO.js
+++ app/infra/produtosDAO.js
@@ -5,10 +5,14 @@ class ProdutosDAO {
     }
 
     lista(callback) {
-        this._connection.query('select * from livros', (err, result) => {
-            callback(err, result);
-        });
+        this._connection.query('select * from livros', callback);
     };
+
+    salva(produto, callback) {
+        this._connection.query('insert into livros set ?', produto, callback);
+        // Caso o json não estivesse com os mesmos nomes das colunas teríamos que especifica-las da seguinte forma:
+        // this._connection.query('insert into produtos (titulo, preco, descricao) values (?, ?, ?)', [produto.titulo, produto.preco, produto.descricao], callback);
+    }
 }
 
 module.exports = () => {
--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,11 +1,32 @@
+const name = 'produtos';
+const route = `/${name}`;
+
 module.exports = (app) => {
-    return app.get('/produtos', (req, res) => {
+    app.get(`${route}`, (req, res) => {
         const connection = app.infra.connectionFactory();
         const produtosDAO = new app.infra.produtosDAO(connection);
         
         produtosDAO.lista((err, result) => {
             err && console.error(err);
-            res.render("produtos/lista", { produtos: (result || []) });
+            res.render(`${name}/lista`, { produtos: (result || []) });
+        });
+
+        connection.end();
+    });
+
+    app.get(`${route}/form`, (req, res) => {
+        res.render(`${name}/form`);
+    });
+
+    app.post(`${route}/salva`, (req, res) => {
+        const produto = req.body;
+
+        const connection = app.infra.connectionFactory();
+        const produtosDAO = new app.infra.produtosDAO(connection);
+
+        produtosDAO.salva(produto, (err, result) => {
+            err && console.error(err);
+            res.redirect(`${route}`);
         });
 
         connection.end();
