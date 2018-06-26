3b7bfb0bebaedc0cf78dcd468b5f764be967775a
Giuliano Gusmão <gusmaoggg@gmail.com>
2018-06-26 19:32:45 -0300

    Realizando tests com o supertest

--- test/produtos.js
+++ test/produtos.js
@@ -1,17 +1,13 @@
-const Produto = require('../Produtos.model');
-const assert = require('assert');
+var express = require('../config/express')();
+var request = require('supertest')(express);
 
 describe('#ProdutosController', () => {
+
     it('#listagem de produtos json', (done) => {
-        new Produto().lista((res) => {
-            // verifica se foi retornado o status 200
-            assert.equal(res.statusCode, 200);
-
-            // verifica se o content-type foi no formato json
-            assert.equal(res.headers['content-type'], 'application/json; charset=utf-8');
-
-            // notifica o mocha que o callback foi finalizado
-            done();
-        });
+        request.get('/produtos')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, done);
     });
+
 });
